1547dd314122c261f24fc842307f00cdcc7bd4a0
DeckHunter <hunterbr100@gmail.com>
2022-06-23 07:48:48 -0300

    Novas auteracoes

--- lib/main.dart
+++ lib/main.dart
@@ -29,40 +29,143 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-      LerJson();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: SingleChildScrollView(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Usuario(context),
+          Column(
+            children: ListaDeMoedas(context),
+          )
+        ]),
       ),
     );
   }
+}
+
+List<Widget> ListaDeMoedas(context) {
+  List<Widget> coins = [];
+  for (int i = 0; i != 8; i++) {
+    var coin = InkWell(
+      onTap: () {
+        //Get.toNamed('/editarAtividade');
+      },
+      child: Container(
+        padding: const EdgeInsets.only(left: 12.0),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 15,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.blueAccent,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 5,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0)),
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: Row(
+              children: [
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Avalanche'),
+                      Divider(),
+                      Text('R 348,77'),
+                    ],
+                  ),
+                ),
+                Image.network(
+                  'https://panda-crypto-images.s3.amazonaws.com/avax.png',
+                  width: MediaQuery.of(context).size.width * 0.12,
+                  height: MediaQuery.of(context).size.height * 0.12,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    coins.add(coin);
+  }
+  return coins;
+}
+
+Widget Usuario(context) {
+  var coin = InkWell(
+    onTap: () {
+      //Get.toNamed('/editarAtividade');
+    },
+    child: Container(
+      padding: const EdgeInsets.only(left: 12.0),
+      margin: const EdgeInsets.symmetric(
+        horizontal: 30,
+        vertical: 15,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.blueAccent,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(10.0),
+              bottomRight: Radius.circular(10.0)),
+          color: Colors.white,
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Todas as moedas'),
+                    Divider(),
+                    Text('c0inS-13435-2342-zksh-34556'),
+                    Divider(),
+                    Text('348,77'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+  return coin;
 }
--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,1 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+class HomeCoins extends StatefulWidget {
+  const HomeCoins({Key? key}) : super(key: key);
+
+  @override
+  State<HomeCoins> createState() => _HomeCoinsState();
+}
+
+class _HomeCoinsState extends State<HomeCoins> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+List<Widget> ListaDeMoedas() {
+  List<Widget> coins = [];
+  for (int i = 0; i != 8; i++) {
+    var coin = InkWell(
+      onTap: () {
+        //Get.toNamed('/editarAtividade');
+      },
+      child: Container(
+        padding: const EdgeInsets.only(left: 12.0),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 2,
+          vertical: 8,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.amber,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 5,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0)),
+            color: Colors.red,
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+            child: Row(
+              children: [
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Moeda'),
+                      Divider(),
+                      Text('Descrição'),
+                    ],
+                  ),
+                ),
+                Card(
+                  color: Colors.amber,
+                  elevation: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Icon(Icons.edit),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    coins.add(coin);
+  }
+  return coins;
+}
